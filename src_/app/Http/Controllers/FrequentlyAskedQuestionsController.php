2f0454407246d3ed4c1bca99fff578ad1d40a127
kamiyama622 <k-kamiyama@DESKTOP-5M88SOR.localdomain>
2022-05-30 22:01:07 +0900

    change the session comfig for solve error 419

--- src_/app/Http/Controllers/FrequentlyAskedQuestionsController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers;
-use App\Http\Controllers\Controller;
-
-class FrequentlyAskedQuestionsController extends Controller{
-   public function show(){
-       return view('faq',);
-	}
-}
--- src_/resources/views/errors/404.blade.php
+++
@@ -1,9 +0,0 @@
-@include("layouts.common")
-@include("layouts.header")
-<main>
-    <div style="text-align:center">
-        <h1>Page Not Found.</h1>
-        <a href="{{ url('/') }}">Back to Top Page</a>
-    </div>
-@include("layouts.footer")
-</main>
