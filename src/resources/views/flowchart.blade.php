efee0dad6873dbed01f7a5f6915892d5703a3518
Masaru <hittec9609@icloud.com>
2022-06-03 14:33:32 +0900

    flowchart make btn

--- src/resources/views/flowchart.blade.php
+++ src/resources/views/flowchart.blade.php
@@ -2,43 +2,62 @@
 @include("layouts.flowchart_header")
 
     <div class="flowchart_page_wrapper">
-
-        <div class="flowchart_left_nav">
-            <div class="user_project_box box_flame"></div>
-            <div class="plus_btn_wrapper">
-                <a class="plus_btn" type="button" href=""><i class="bi bi-plus-circle"style="font-size: 2rem"></i></a>
+        <div id="titleAreaWrapper">
+            <div id="titleArea">
+                <div class="user_project_box box_flame"></div>
+                <div class="plus_btn_wrapper">
+                    <a class="plus_btn" type="button" href=""><i class="bi bi-plus-circle"style="font-size: 2rem"></i></a>
+                </div>
             </div>
         </div>
 
-        <div class="flowchart_create_box box_shadow box_flame">
-        </div>
+        <div id="flowChartArea"></div>
 
-        <div class="flowchart_right_nav">
+        <div id="functionArea">
             <div id="confirmationArea">
-                <div id="startEnd" class="flowchart_items"></div>
-                <div id="branch" class="flowchart_items"></div>
-                <div id="process" class="flowchart_items"></div>
-                <div id="input" class="flowchart_items"></div>
-                <div id="loopStart" class="flowchart_items"></div>
-                <div id="loopEnd" class="flowchart_items"></div>
-                <div id="display" class="flowchart_items"></div>
-                <div id="connectOut" class="flowchart_items"></div>
-                <div id="connectIn" class="flowchart_items"></div>
+                <div id="startEnd" class="flowchart_items">
+                    <div class="fontClass">start&stop</div>
+                </div>
+                <div id="branch" class="flowchart_items">
+                    <div id="branchFontReset">
+                        <p>if</p>
+                    </div>
+                </div>
+                <div id="process" class="flowchart_items">
+                    <div class="fontClass">process</div>
+                </div>
+                <div id="input" class="flowchart_items">
+                    <div class="fontClass">input</div>
+                </div>
+                <div id="loopStart" class="flowchart_items">
+                    <div class="fontClass">LoopStart</div>
+                </div>
+                <div id="loopEnd" class="flowchart_items">
+                    <div class="fontClass">LoopEnd</div>
+                </div>
+                <div id="display" class="flowchart_items">
+                </div>
+                <div id="connectOut" class="flowchart_items">
+                    <div class="fontClass">C</div>
+                </div>
+                <div id="connectIn" class="flowchart_items">
+                    <div class="fontClass">I</div>
+                </div>
             </div>
 
-            <div id="btnArea" box_shadow">
-                <div class="box_type box_type1 box_flame"></div>
-                <div class="box_type box_type1 box_flame"></div>
-                <div class="box_type box_type2 box_flame"></div>
-                <div class="box_type box_type2 box_flame"></div>
-                <div class="box_type box_type2 box_flame"></div>
-            </div>
+            <div id="btnArea">
+                <div id="functionBtn">
+                    <div class="box_type box_type1 box_flame"></div>
+                    <div class="box_type box_type1 box_flame"></div>
+                    <div class="box_type box_type2 box_flame"></div>
+                    <div class="box_type box_type2 box_flame"></div>
+                    <div class="box_type box_type2 box_flame"></div>
+                </div>
 
-            <div class="how_to_btn_box">
+                <div class="howToBtn">
                 <a type="button" class="btn btn-outline-info how_to_btn" href="{{ url('/') }}"></i>How To</a>
+                </div>
             </div>
-
-
         </div>
     </div>
 
