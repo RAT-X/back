c779fee3f88d532a6832dc31a1f67700a346ac54
Masaru <hittec9609@icloud.com>
2022-06-06 22:45:43 +0900

    nameChange

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,21 +1,22 @@
+
 @include("layouts.head")
 
-<div class="page_wrapper">
-    <div class="welcome_message">
+<div class="mainPageWrapper">
+    <div class="welcomeMessageBox">
         <h2>こんにちは User.</h2>
     </div>
 
-    <div class="welcome_wrapper">
-        <div class="create_message">
+    <div class="createBoxWrapper">
+        <div class="createMessageBox">
             <h1>フローチャートの<br>作成を始めてください</h1>
         </div>
 
-        <div class="choice_btn_wrapper">
-            <div class="choice_btn">
-                <a type="button" class="btn btn-success choice_btn" href="{{ url('/load_flowchart') }}"><i class="bi bi-arrow-up-right-square" style="font-size: 1.5rem"></i> &ensp;作成済みの<br>プロジェクトを開く</a>
+        <div class="createBtnWrapper">
+            <div class="createBtnBox">
+                <a type="button" class="btn btn-success" href="{{ url('/load_flowchart') }}"><i class="bi bi-arrow-up-right-square" style="font-size: 1.5rem"></i> &ensp;作成済みの<br>プロジェクトを開く</a>
             </div>
-            <div class="choice_btn">
-                <a type="button" class="btn btn-primary choice_btn" href="{{ url('/create_flowchart') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1.5rem"></i> &ensp;新規作成</a>
+            <div class="createBtnBox">
+                <a type="button" class="btn btn-primary" href="{{ url('/create_flowchart') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1.5rem"></i> &ensp;新規作成</a>
             </div>
             <form action="{{ route('logout') }}" method="post">
                 @csrf
