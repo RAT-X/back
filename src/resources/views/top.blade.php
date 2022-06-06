3598f06a119431fb667a4084b46641f7b4a6034c
Masaru <hittec9609@icloud.com>
2022-06-06 21:11:32 +0900

    nameChange

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -1,17 +1,18 @@
 @include("layouts.head")
 
-<div class="page_wrapper">
-    <div class="message_wrapper">
+<div class="mainPageWrapper">
+
+    <div class="messageBox">
         <h1>chartmanへようこそ</h1>
     </div>
-    <div class="login_wrapper">
-        <div class="login_box">
-            <a type="button" class="btn btn-success login_register_btn" href="{{ url('/login') }}"><i class="bi bi-arrow-up-right-square" style="font-size: 1.5rem"></i> &ensp;ログイン</a>
-            <a type="button" class="btn btn-primary  login_register_btn" href="{{ url('/register') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1.5rem"></i> &ensp;新規登録</a>
 
-
+    <div class="loginBoxWrapper">
+        <div class="loginBox">
+            <a type="button" class="btn btn-success loginBtn" href="{{ url('/login') }}"><i class="bi bi-arrow-up-right-square" style="font-size: 1.5rem"></i> &ensp;ログイン</a>
+            <a type="button" class="btn btn-primary  loginBtn" href="{{ url('/register') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1.5rem"></i> &ensp;新規登録</a>
         </div>
     </div>
+
 </div>
 
 @include("layouts.script")
