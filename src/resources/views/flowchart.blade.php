6bce01ef34a55fd301de1751c36d914a19251e04
Masaru <hittec9609@icloud.com>
2022-06-06 14:44:59 +0900

    flowchartpage

--- src/resources/views/flowchart.blade.php
+++ src/resources/views/flowchart.blade.php
@@ -2,45 +2,72 @@
 @include("layouts.flowchart_header")
 
     <div class="flowchart_page_wrapper">
-        <div id="titleAreaWrapper">
-            <div id="titleArea">
-                <div class="user_project_box box_flame"></div>
-                <div class="plus_btn_wrapper">
-                    <a class="plus_btn" type="button" href=""><i class="bi bi-plus-circle"style="font-size: 2rem"></i></a>
-                </div>
+        <div id="titleArea">
+            <div class="user_project_box box_flame"></div>
+            <div class="plus_btn_wrapper">
+                <a class="plusBtn" href=""><i class="bi bi-plus-circle"style="font-size: 2rem"></i></a>
             </div>
         </div>
 
-        <div id="flowChartArea"></div>
+        <div id="flowChartArea">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga illum aliquid,<br> ipsa maxime, eaque porro doloremque repudiandae at hic,<br> dignissimos sunt nihil quas ab laudantium expedita! Odio explicabo consequatur qui.</p>
+            <br><br>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti magnam fugiat eligendi praesentium expedita quia saepe minima, repellendus molestiae ullam omnis beatae assumenda doloribus nulla, temporibus officiis, voluptatem sunt iure.</p>
+        </div>
 
         <div id="functionArea">
             <div id="confirmationArea">
-                <div id="startEnd" class="flowchart_items">
+                <div id="startEnd" class="flowChartItems">
                     <div class="fontClass">start&stop</div>
                 </div>
-                <div id="branch" class="flowchart_items">
+                <div id="branch" class="flowChartItems">
                     <div id="branchFontReset">
                         <p>if</p>
                     </div>
                 </div>
-                <div id="process" class="flowchart_items">
+                <div id="process" class="flowChartItems">
                     <div class="fontClass">process</div>
                 </div>
-                <div id="input" class="flowchart_items">
+                <div id="input" class="flowChartItems">
                     <div class="fontClass">input</div>
                 </div>
-                <div id="loopStart" class="flowchart_items">
+                <div id="loopStart" class="flowChartItems">
                     <div class="fontClass">LoopStart</div>
                 </div>
-                <div id="loopEnd" class="flowchart_items">
+                <div id="loopEnd" class="flowChartItems">
                     <div class="fontClass">LoopEnd</div>
                 </div>
-                <div id="display" class="flowchart_items">
+                <div id="display" class="flowChartItems">
                 </div>
-                <div id="connectOut" class="flowchart_items">
+                <div id="connectOut" class="flowChartItems">
                     <div class="fontClass">C</div>
                 </div>
-                <div id="connectIn" class="flowchart_items">
+                <div id="connectIn" class="flowChartItems">
                     <div class="fontClass">I</div>
                 </div>
             </div>
@@ -59,6 +86,7 @@
                 </div>
             </div>
         </div>
+
     </div>
 
 @include("layouts.script")
