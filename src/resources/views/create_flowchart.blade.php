3d446b2676756e94859a0a435c566f17a8361731
Masaru <hittec9609@icloud.com>
2022-06-07 10:06:30 +0900

    nameChange

--- src/resources/views/create_flowchart.blade.php
+++ src/resources/views/create_flowchart.blade.php
@@ -1,20 +1,18 @@
 @include("layouts.head")
 
-<div class="page_wrapper">
-    <div class="message_wrapper">
+<div class="mainPageWrapper">
+    <div class="messageBox">
         <h1>作成するアプリ名を入力してください</h1>
     </div>
 
-    <div class="login_wrapper">
-    <div class="project_box">
-        <p class="project_title">プロジェクトのタイトル</p>
-        <div class="form-floating mb-3 project_name_btn">
-        <input type="email" class="form-control" id="floatingInput">
-        </div>
+    <div class="createProjectWrapper">
+        <div class="projectBox">
+            <p class="projectTitle">プロジェクトのタイトル</p>
+            <div class="form-floating mb-3 projectNameBtn">
+            <input type="email" class="form-control" id="floatingInput">
+            </div>
 
-        <div class="create_btn_wrapper">
-        <a type="button" class="btn btn-primary create_project_btn" href="{{ url('/flowchart') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1rem"></i>&ensp;作成</a>
-        </div>
-    </div>
+            <a type="button" class="btn btn-primary create_project_btn" href="{{ url('/flowchart') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1rem"></i>&ensp;作成</a>
+            </div>
     </div>
 </div>
--- src/resources/views/load_flowchart.blade.php
+++ src/resources/views/load_flowchart.blade.php
@@ -8,13 +8,12 @@
   <div class="createProjectWrapper">
     <div class="projectBox">
       <p class="projectTitle">プロジェクトのタイトル</p>
+      
       <div class="form-floating mb-3 projectNameBtn">
         <input type="email" class="form-control" id="floatingInput">
       </div>
 
-      <div class="create_btn_wrapper">
         <a type="button" class="btn btn-primary projectCreateBtn" href="{{ url('/flowchart') }}"><i class="bi bi-arrow-down-right-square" style="font-size: 1rem"></i>&ensp;作成</a>
-      </div>
     </div>
   </div>
 </div>
