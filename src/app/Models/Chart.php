ba302b4e6e181bc6b57adf3ce42bbfd344ef525e
Masaru <hittec9609@icloud.com>
2022-06-09 09:36:24 +0900

    Chartモデルの作成, Charts_tableと紐づけ

---
+++ src/app/Models/Chart.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chart extends Model
+{
+    use HasFactory;
+
+     // charts_tableと紐づけ
+     protected $fillable = [
+        'user_id',
+        'project_title',
+    ];
+}
