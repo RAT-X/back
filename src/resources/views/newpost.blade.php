86916adad98a014e6abe94794b8be6abc6d3a814
Masaru <hittec9609@icloud.com>
2022-06-09 09:43:38 +0900

    newpostのviewとcontrollerの作成

---
+++ src/resources/views/newpost.blade.php
@@ -0,0 +1,9 @@
+@include("layouts.head")
+
+<div class="mainPageWrapper">
+    <form action="action.php" method="post">
+    名前: <input type="text" name="name" />
+    コメント: <input type="text" name="coment" />
+    <input type="submit" />
+    </form>
+</div>
