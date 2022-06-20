52cfd7ff7f215795c77a215c28ef62a67b187413
Masaru <hittec9609@icloud.com>
2022-06-20 18:29:33 +0900

    1対多でデータの取り出し

--- src/resources/views/project_list.blade.php
+++ src/resources/views/project_list.blade.php
@@ -5,15 +5,23 @@
   <h1>プロジェクト一覧</h1><br>
 
   @foreach ($projects as $project)
-  <div class="userProjectBox box_flame">
-  <a href="{{ route('project.detail', ['id' => $project->id, 'project_title' => $project->project_title]) }}">
-    Number: {{$project->id}}<br>
-    Title: {{ $project->project_title }}
-  </a>
-</div>
+    <a href="{{ route('project.detail', ['id' => $project->id, 'project_title' => $project->project_title]) }}">
+      <div class="userProjectBox box_flame">
+        {{ $project->getData() }}
+      </div>
+    </a>
+  @endforeach
 
 
-    @endforeach
+
+  @foreach ($projects as $project)
+    <a href="{{ route('project.detail', ['id' => $project->id, 'project_title' => $project->project_title]) }}">
+      <div class="userProjectBox box_flame">
+        Number: {{$project->id}}<br>
+        Title: {{ $project->project_title }}
+      </div>
+    </a>
+  @endforeach
 
   <div>
     <a href={{ route('project.new') }} class="btn btn-outline-primary">新しいプロジェクト</a>
--- src/resources/views/user_project.blade.php
+++ src/resources/views/user_project.blade.php
@@ -5,7 +5,7 @@
 
   <h1>Project title: {{ $project->project_title }}<br></h1>
 
-  <p>Project number:{{ $project->project_number }}</p>
+  <p>Project number:{{ $project->id }}</p>
   <p>Project Comment:<br>{{ $project->comment }}</p>
 
     <a href={{ route('project.list') }} class="btn btn-outline-primary">一覧に戻る</a>
